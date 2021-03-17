8e3ef5d07a5b4b2b3fd721ef1096aea3191f98b4
Trexychan <trexdomobutter@gmail.com>
2021-03-17 01:49:38 -0400

    Changed enemy code drastically o_o
    EnemyController controls enemy health and other similar functions that are shared between enemy types

---
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public EnemyType maxHealth;
+    int currentHealth;
+
+    void Start() {
+        this.currentHealth = (int)maxHealth;
+        Debug.Log(this.currentHealth);
+    }
+    
+    public void TurnToFacePlayer(Transform pos) {
+        if (pos.transform.position.x > this.gameObject.transform.position.x) {
+            this.gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+        } else {
+            this.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+    public virtual void Die() {
+        this.gameObject.SetActive(false);
+    }
+
+    public void TakeDamage(int damage) {
+        this.currentHealth -= damage;
+        Debug.Log(this.currentHealth);
+        if (this.currentHealth <= 0) {
+            Die();
+        }
+    }
+}
+
+public enum EnemyType
+{
+    MINOR=2,
+    BEEMON=3,
+    RATTANK=10
+}
