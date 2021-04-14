eb88e4ed9b242bc7eb358c8192b1870f95c9799f
Trexychan <trexdomobutter@gmail.com>
2021-04-14 11:01:44 -0400

    Added shotgun anims and shooting

--- Assets/Scripts/Guns/Shotgun.cs
+++ Assets/Scripts/Guns/Shotgun.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class Shotgun : RaycastGun
 {
     public int shotgunPellets = 5;
-    public Shotgun(StatePlayerController player, Transform firePoint, GameObject hitEffect, AudioClip fireSound, LineRenderer renderer, RuntimeAnimatorController animatorController, Sprite icon) {
+    GameObject shot_trail;
+    public Shotgun(StatePlayerController player, Transform firePoint, GameObject hitEffect, AudioClip fireSound, GameObject shot_trail, RuntimeAnimatorController animatorController, Sprite icon) {
         this.size = Size.NORMAL;
-        this.shotCost = 4f;
+        this.shotCost = 3f;
         this.damage = 1;
-        this.fireRate = 0.2f;
+        this.fireRate = 1f;
         this.firePt = firePoint;
-        this.bulletTrail = renderer;
+        this.bulletTrail = shot_trail.GetComponent<LineRenderer>();
+        this.shot_trail = shot_trail;
         this.hitEffect = hitEffect;
         this.fireSound = fireSound;
         this.player = player;
         this.animController = animatorController;
-        this.maxRange = 10f;
+        this.maxRange = 3f;
         this.icon = icon;
     }
 
@@ -25,26 +27,35 @@ public class Shotgun : RaycastGun
     {
         for (int i=0; i<shotgunPellets; i++)
         {
+            GameObject temptrail = (GameObject)Instantiate(shot_trail, firePt.position, Quaternion.identity);
+            LineRenderer temptrailrenderer = temptrail.GetComponent<LineRenderer>();
             Vector3 temp = firePt.right;
             temp.y += Random.Range(-0.3f,0.3f);
-            RaycastHit2D hitInfo = Physics2D.Raycast(firePt.position, temp);
+            RaycastHit2D hitInfo = Physics2D.Raycast(firePt.position, temp, maxRange, ~layermask);
             if (hitInfo)
             {
-                bulletTrail.SetPosition(0,firePt.position);
-                bulletTrail.SetPosition(1,hitInfo.point);
+                temptrailrenderer.SetPosition(0,firePt.position);
+                temptrailrenderer.SetPosition(1,hitInfo.point);
 
-                GameObject new_hit = (GameObject)Instantiate(hitEffect, hitInfo.point, Quaternion.identity);
-                Destroy(new_hit, 0.267f);
+                EnemyController target = hitInfo.collider.gameObject.GetComponent<EnemyController>();
+                if (target) {
+                    target.TakeDamage(this.damage);
+                    GameObject new_hit = (GameObject)Instantiate(hitEffect, hitInfo.point, Quaternion.identity);
+                    Destroy(new_hit, 0.267f);
+                } else {
+                    Instantiate(player.deflectshotEffect,hitInfo.point,Quaternion.identity);
+                }
 
             } else 
             {
                 
-                bulletTrail.SetPosition(0,firePt.position);
-                bulletTrail.SetPosition(1,firePt.position + temp*100);
+                temptrailrenderer.SetPosition(0,firePt.position);
+                temptrailrenderer.SetPosition(1,firePt.position + temp*maxRange);
             }
-            player.showBulletTrail(bulletTrail);
+            Destroy(temptrail, 0.05f);
         }
         player.playSound(fireSound);
+        player.DecreasePlayerCurrentHealth(shotCost);
 
     }
 }
